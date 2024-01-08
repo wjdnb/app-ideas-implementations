5a619041d6dad776239ed6b0a6006791f832a60e
wjdnb <598777720@qq.com>
2024-01-08 23:05:28 +0800

    feat: add flip image

--- src/pages/FlipImage.jsx
+++ src/pages/FlipImage.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import { UpTwo, DownTwo, RightTwo, LeftTwo } from "@icon-park/react";
+import NormalButton from "../components/Button";
+
 function FlipImage() {
-  return <div>FlipImage</div>;
+  const [address, serAddress] = useState(
+    "https://images.pexels.com/photos/2335126/pexels-photo-2335126.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  );
+
+  return (
+    <div className="mt-10">
+      <div className="w-96 h-96 relative select-none">
+        <div className="absolute left-44 -top-12 cursor-pointer">
+          <UpTwo
+            theme="filled"
+            size="32"
+            fill="#8b5cf6"
+            strokeWidth={3}
+            strokeLinecap="square"
+          />
+        </div>
+        <div className="absolute top-44 -left-12 cursor-pointer">
+          <LeftTwo
+            theme="filled"
+            size="32"
+            fill="#8b5cf6"
+            strokeWidth={3}
+            strokeLinecap="square"
+          />
+        </div>
+        <div className="absolute top-44 -right-12 cursor-pointer">
+          <RightTwo
+            theme="filled"
+            size="32"
+            fill="#8b5cf6"
+            strokeWidth={3}
+            strokeLinecap="square"
+          />
+        </div>
+        <div className="absolute -bottom-12 left-44 cursor-pointer">
+          <DownTwo
+            theme="filled"
+            size="32"
+            fill="#8b5cf6"
+            strokeWidth={3}
+            strokeLinecap="square"
+          />
+        </div>
+        <img src={address} alt="" className="w-96 h-96" />
+      </div>
+      <div className="flex items-center mt-20">
+        <input
+          type="text"
+          className="outline-none h-11 w-80 text-xl border border-solid rounded"
+        />
+        <NormalButton>替换</NormalButton>
+      </div>
+    </div>
+  );
 }
 
 export default FlipImage;
